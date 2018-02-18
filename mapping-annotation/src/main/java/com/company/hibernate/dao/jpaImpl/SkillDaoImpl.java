107ca248949588cf45852498e5a44d0127f19dbc
Sasha Kaminska <sasha249133@ukr.net>
2018-02-18 15:28:48 +0200

    mapping-annotations dao

--- mapping-annotation/src/main/java/com/company/hibernate/dao/jpaImpl/SkillDaoImpl.java
+++ mapping-annotation/src/main/java/com/company/hibernate/dao/jpaImpl/SkillDaoImpl.java
@@ -1,4 +1,37 @@
 package com.company.hibernate.dao.jpaImpl;
 
-public class SkillDaoImpl {
+import com.company.hibernate.dao.AbstractJpaDao;
+import com.company.hibernate.model.Skill;
+import java.util.List;
+
+public class SkillDaoImpl extends AbstractJpaDao<Skill> {
+
+    public SkillDaoImpl(){
+        super.setClass(Skill.class);
+    }
+
+    @Override
+    public void save(Skill skill) {
+        super.save(skill);
+    }
+
+    @Override
+    public Skill getById(Long id) {
+        return super.getById(id);
+    }
+
+    @Override
+    public void remove(Long id) {
+        super.remove(id);
+    }
+
+    @Override
+    public List<Skill> getAll() {
+        return super.getAll();
+    }
+
+    @Override
+    public void update(Skill skill) {
+        super.update(skill);
+    }
 }
