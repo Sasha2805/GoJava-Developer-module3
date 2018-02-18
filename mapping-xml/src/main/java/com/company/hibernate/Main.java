150437eb68b1efee19a2a45015324b2d2239d66e
Sasha Kaminska <sasha249133@ukr.net>
2018-02-18 15:16:56 +0200

    mapping-xml util and main

--- mapping-xml/src/main/java/com/company/hibernate/Main.java
+++ mapping-xml/src/main/java/com/company/hibernate/Main.java
@@ -1,4 +1,11 @@
-package com.company.hibernate.util;
+package com.company.hibernate;
+
+import com.company.hibernate.view.MainView;
 
 public class Main {
+    public static void main(String[] args) {
+        MainView.getView();
+        MainView.SCANNER.close();
+        System.exit(0);
+    }
 }
